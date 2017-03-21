094839d70e9280263444dfd2bb5083ea0f808340
L-M-ICA40511 <L-M-ICA40511@users.noreply.github.com>
2017-03-21 11:09:41 +1100

    Update Location.php
    Add event prefixes

--- app/code/community/Demac/MultiLocationInventory/Model/Location.php
+++ app/code/community/Demac/MultiLocationInventory/Model/Location.php
@@ -6,6 +6,20 @@
 class Demac_MultiLocationInventory_Model_Location extends Mage_Core_Model_Abstract
 {
     /**
+     * Model event prefix
+     *
+     * @var string
+     */
+    protected $_eventPrefix = 'multilocationinventory_location';
+
+    /**
+     * Name of the event object
+     *
+     * @var string
+     */
+    protected $_eventObject = 'location';
+    
+    /**
      * Init model
      */
     public function _construct()
